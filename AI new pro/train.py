a2d25ac50c5239e9b40392995a28a790e80794a0
Anshu <100580064+anshu80verma@users.noreply.github.com>
2022-11-24 22:07:25 +0530

    Update train.py

--- AI new pro/train.py
+++ AI new pro/train.py
@@ -37,4 +37,39 @@ for(pattern_sentence , tag) in xy:
     label = tags.index(tag)
     y_tarin.append(label)
 
-    x_train = np.array
+x_train = np.array(x_tarin)
+y_train = np.array(y_train)
+
+num_epochs =1000
+batch_size = 8
+learning_rate = 0.001
+hidden_size = 8
+input_size = len(x_train[0])
+output_size = len(tags)
+
+print("Train the model...")
+
+
+class chatDataset(dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data  = x_train
+        self.y_data = y_train
+
+
+    def __getitem__(self,index):
+        return self.x_data[index],self.y_data[index]
+
+
+    def __len__(self):
+        return self.n_samples
+
+
+Dataset = chatDataset()
+
+train_loader = dataloader(dataset = dataset,batch_size = batch_size,shuffle = True , num_worker = 0)
+Device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model  =    NerualNet(input_size,hidden_size,output_size).to(device=Device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+    
